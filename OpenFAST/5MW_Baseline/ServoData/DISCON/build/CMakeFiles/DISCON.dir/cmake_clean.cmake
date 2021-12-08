1b8f7ac36052ff3571451b9334a7c1431f9755a0
James Xu <91702438+jamesterryxu@users.noreply.github.com>
2021-12-07 21:14:05 -0800

    Add files via upload

---
+++ OpenFAST/5MW_Baseline/ServoData/DISCON/build/CMakeFiles/DISCON.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DISCON.dir/DISCON.F90.o"
+  "DISCON.pdb"
+  "DISCON.dll"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/DISCON.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenFAST/5MW_Baseline/ServoData/DISCON_ITI/build/CMakeFiles/DISCON_ITIBarge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DISCON_ITIBarge.dir/DISCON_ITIBarge.F90.o"
+  "DISCON_ITIBarge.pdb"
+  "DISCON_ITIBarge.dll"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/DISCON_ITIBarge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
